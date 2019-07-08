93e78b7be78e280d5c03e8b61f28337f690cf02d
JHeeg <heeggggg@gmail.com>
2019-07-08 17:38:05 +0900

    07.08

--- js/common_ui.js
+++ js/common_ui.js
@@ -6,6 +6,17 @@ $(document).on('click', 'a[href="#"]', function(e) {
 $(document).ready(function() {
     showDown();
     menuToggle();
+    // multiScroll();
+
+    // 윈도우 화면이 1550px보다 작을 경우 gnb가 자동으로 줄어들도록 설정
+    $(window).on('resize', function() {
+        windowWidth = $(window).width();
+        if (windowWidth < 1550) {
+            $('#header').addClass('on');
+            $('#gnb').addClass('on');
+            $('#main').addClass('on');
+        }
+    });
 });
 
 // activity 영역의 다운 화살표를 누르면 해당 내용이 보여지는 함수
@@ -13,11 +24,13 @@ function showDown() {
     $('a.slide-down').on('click', function() {
         $(this).parent().next('.content').stop().slideToggle('fast');
     });
-
 }
+// 메뉴버튼 토글
 function menuToggle() {
     $('#header a.menu').on('click', function() {
-        $(this).toggleClass('toggle');
+        $(this).parent().toggleClass('on');
         $('#gnb').toggleClass('on');
+        $('#main').toggleClass('on');
     });
+
 }
